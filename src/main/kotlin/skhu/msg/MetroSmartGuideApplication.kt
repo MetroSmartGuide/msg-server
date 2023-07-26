af41f54eca9632fdc0b21af77ef5bca2bebb9eaa
YehyeokBang <qkddpgur318@gmail.com>
2023-07-26 17:27:04 +0900

    스프링 부트 프로젝트 생성

---
+++ src/main/kotlin/skhu/msg/MetroSmartGuideApplication.kt
@@ -0,0 +1,11 @@
+package skhu.msg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MetroSmartGuideApplication
+
+fun main(args: Array<String>) {
+    runApplication<MetroSmartGuideApplication>(*args)
+}
---
+++ src/test/kotlin/skhu/msg/MetroSmartGuideApplicationTests.kt
@@ -0,0 +1,13 @@
+package skhu.msg
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MetroSmartGuideApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
