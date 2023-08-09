5837fc7a8e9884a4833d9fad7600daf2ec1e78ec
YehyeokBang <qkddpgur318@gmail.com>
2023-08-09 20:55:35 +0900

    [#19] MethodArgumentNotValidException 핸들링 및 ErrorCode 추가

--- src/main/kotlin/skhu/msg/global/exception/handler/ExceptionHandler.kt
+++ src/main/kotlin/skhu/msg/global/exception/handler/ExceptionHandler.kt
@@ -1,7 +1,9 @@
 package skhu.msg.global.exception.handler
 
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import skhu.msg.global.exception.ErrorCode
 import skhu.msg.global.exception.GlobalException
 import skhu.msg.global.exception.response.ResponseException
 
@@ -9,11 +11,24 @@ import skhu.msg.global.exception.response.ResponseException
 class ExceptionHandler {
 
     @ExceptionHandler(GlobalException::class)
-    fun handleGlobalException(e: GlobalException): ResponseException {
+    fun handleGlobalException(ex: GlobalException): ResponseException {
         return ResponseException.create(
-            statusCode = e.error.status.value(),
-            error = e.error,
-            message = e.error.message,
+            statusCode = ex.error.status.value(),
+            error = ex.error,
+            message = ex.error.message,
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationException(ex: MethodArgumentNotValidException): ResponseException {
+        val errorReason= ex.bindingResult.fieldError?.field ?: ""
+        val errorMessage = ErrorCode.INVALID_REQUEST_INPUT.message
+        val customErrorMessage = "$errorMessage / $errorReason"
+
+        return ResponseException.create(
+            statusCode = 400,
+            error = ErrorCode.INVALID_REQUEST_INPUT,
+            message = customErrorMessage,
         )
     }
 
