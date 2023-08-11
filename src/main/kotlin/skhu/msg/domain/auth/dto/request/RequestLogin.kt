a971e48b97a72c39fbcb70fd11f32c5a1755d84a
YehyeokBang <qkddpgur318@gmail.com>
2023-08-11 18:49:28 +0900

    Validation 적용을 위해 필드 nullable 타입으로 변경

--- src/main/kotlin/skhu/msg/domain/auth/dto/request/RequestLogin.kt
+++ src/main/kotlin/skhu/msg/domain/auth/dto/request/RequestLogin.kt
@@ -6,11 +6,11 @@ import jakarta.validation.constraints.NotBlank
 data class RequestLogin(
     @field:Email(message = "이메일 형식이 올바르지 않습니다.")
     @field:NotBlank(message = "이메일이 비어있습니다.")
-    var email: String,
+    var email: String? = null,
 
     @field:NotBlank(message = "닉네임이 비어있습니다.")
-    var nickname: String,
+    var nickname: String? = null,
 
     @field:NotBlank(message = "토큰이 비어있습니다.")
-    var uid: String,
+    var uid: String? = null,
 )
