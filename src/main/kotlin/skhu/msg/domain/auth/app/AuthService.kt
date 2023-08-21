6c05d98f431ac31d084ff21ce2111bbf45c7cce7
Yehyeok Bang <qkddpgur318@gmail.com>
2023-08-21 17:17:20 +0900

    [#29] RefreshToken 발급 및 AccessToken 재발급 기능 추가

--- src/main/kotlin/skhu/msg/domain/auth/app/AuthService.kt
+++ src/main/kotlin/skhu/msg/domain/auth/app/AuthService.kt
@@ -1,10 +1,14 @@
 package skhu.msg.domain.auth.app
 
+import jakarta.servlet.http.HttpServletRequest
 import skhu.msg.domain.auth.dto.request.RequestLogin
+import skhu.msg.domain.auth.dto.request.RequestRefresh
 import skhu.msg.domain.auth.dto.response.ResponseToken
 
 interface AuthService {
 
     fun joinOrLogin(requestLogin: RequestLogin): ResponseToken
+
+    fun refreshAccessToken(request: HttpServletRequest, requestRefresh: RequestRefresh): ResponseToken
 
 }
