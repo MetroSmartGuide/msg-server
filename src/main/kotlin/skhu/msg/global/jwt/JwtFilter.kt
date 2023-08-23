d1b6ebb3f4d4db2d97d31988809c20381fd045b8
Yehyeok Bang <qkddpgur318@gmail.com>
2023-08-23 22:48:33 +0900

    [#27] 로그아웃된 토큰 사용불가 처리 기능 추가

--- src/main/kotlin/skhu/msg/global/jwt/JwtFilter.kt
+++ src/main/kotlin/skhu/msg/global/jwt/JwtFilter.kt
@@ -4,22 +4,29 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.util.StringUtils
 import org.springframework.web.filter.GenericFilterBean
 
 class JwtFilter (
-    private val tokenProvider: TokenProvider
+    private val tokenProvider: TokenProvider,
+    private val redisTemplate: RedisTemplate<Any, Any>,
 ): GenericFilterBean() {
 
     override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
-        val token = tokenProvider.resolveToken(request as HttpServletRequest)
+        val accessToken = tokenProvider.resolveToken(request as HttpServletRequest)
 
-        if (request.requestURI != "/api/v1/auth/refresh") {
-            if (StringUtils.hasText(token)) {
-                if (tokenProvider.validateToken(token!!)) {
-                    val authentication: Authentication = tokenProvider.getAuthentication(token!!)
+        if (request.requestURI == "/api/v1/auth/refresh") {
+            chain.doFilter(request, response)
+            return
+        }
+
+        if (StringUtils.hasText(accessToken)) {
+            if (tokenProvider.validateToken(accessToken!!)) {
+                if (redisTemplate.opsForValue().get(accessToken) == null) {
+                    val authentication: Authentication = tokenProvider.getAuthentication(accessToken)
                     SecurityContextHolder.getContext().authentication = authentication
                 }
             }
