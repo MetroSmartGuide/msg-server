f303eb39c8eaa5d8ebc036e7a6458921f8108268
Yehyeok Bang <qkddpgur318@gmail.com>
2023-08-31 22:33:41 +0900

    [#40] caffeine 의존성 추가

---
+++ src/test/kotlin/skhu/msg/domain/metro/app/impl/OpenApiConnectorTest.kt
@@ -0,0 +1,17 @@
+package skhu.msg.domain.metro.app.impl
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class OpenApiConnectorTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @Test
+    fun getRealTimeCongestion() {
+    }
+}
