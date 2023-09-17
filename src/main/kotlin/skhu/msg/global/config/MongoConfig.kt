8677794c760cd1fd6acfddc1496de2e6a653d3ba
Yehyeok Bang <qkddpgur318@gmail.com>
2023-09-18 03:52:16 +0900

    로깅 레벨 설정 및 Dockerfile 변경

--- src/main/kotlin/skhu/msg/global/config/MongoConfig.kt
+++ src/main/kotlin/skhu/msg/global/config/MongoConfig.kt
@@ -2,6 +2,7 @@ package skhu.msg.global.config
 
 import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
+import com.mongodb.MongoCredential
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
 import org.springframework.beans.factory.annotation.Value
@@ -13,6 +14,8 @@ class MongoConfig: AbstractMongoClientConfiguration() {
 
     @Value("\${spring.data.mongodb.uri}") private lateinit var mongoUri: String
     @Value("\${spring.data.mongodb.database}") private lateinit var databaseName: String
+    @Value("\${spring.data.mongodb.username}") private lateinit var username: String
+    @Value("\${spring.data.mongodb.password}") private lateinit var password: String
 
     override fun getDatabaseName(): String {
         return databaseName
@@ -21,8 +24,11 @@ class MongoConfig: AbstractMongoClientConfiguration() {
     override fun mongoClient(): MongoClient {
         val connectionString = ConnectionString(mongoUri)
 
+        val credentials = MongoCredential.createCredential(username, databaseName, password.toCharArray())
+
         val mongoClientSettings = MongoClientSettings.builder()
             .applyConnectionString(connectionString)
+            .credential(credentials)
             .build()
 
         return MongoClients.create(mongoClientSettings)
